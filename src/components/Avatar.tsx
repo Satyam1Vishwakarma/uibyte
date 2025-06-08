d71d712f1b92472e6f73527fa86fe86910af6135
Satyam Vishwakarma <satyam.k.vishwakarma@gmail.com>
2025-06-08 11:15:02 +0530

    avatar final

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { twMerge } from "tailwind-merge"
 
 type AvatarProps = {
   src: string;
@@ -12,11 +13,12 @@ export default function Avatar({
   src,
   loading = "lazy",
   fallback = "?",
+  className=""
 }: AvatarProps) {
   const [isloaded, setisloaded] = useState(false);
 
   return (
-    <div className={"relative w-10 h-10 rounded-4xl border border-black/25 overflow-hidden flex justify-center items-center"}>
+    <div className={twMerge("relative w-10 h-10 rounded-4xl border border-black/25 overflow-hidden flex justify-center items-center",className)}>
       <img ///////// image needs to mounted in dom first to fire up onload and error
         src={src}
         alt={fallback}
