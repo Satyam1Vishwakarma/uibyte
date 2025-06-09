a44c2fe2a8e8f464fc599db3284b8c00fe30007e
Satyam Vishwakarma <satyam.k.vishwakarma@gmail.com>
2025-06-09 09:11:45 +0530

    final button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 "use state";
 
 import type React from "react";
+import { useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 type ButtonProps = {
@@ -9,12 +10,20 @@ type ButtonProps = {
 };
 
 export default function Button({ className, children, ...rest }: ButtonProps) {
-  //const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   return (
     <button
-      className={twMerge("bg-black text-white p-2 rounded-2xl w-sm", className)}
-      //animate={{ backgroundColor: clicked ? "#2563eb" : "#000000" }}
+      className={twMerge(
+        `bg-black text-white p-2 rounded-2xl w-sm  ${
+          clicked ? "" : "shadow-2xl shadow-black/65"
+        } `,
+        className
+      )}
+      onMouseUp={() => setClicked(false)}
+      onMouseDown={() => setClicked(true)}
+      onTouchStart={() => setClicked(true)}
+      onTouchEnd={() => setClicked(false)}
       {...rest}
     >
       {children}
