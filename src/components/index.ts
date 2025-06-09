6310f618a45ae23c22a139199c7388020ad004b7
Satyam Vishwakarma <satyam.k.vishwakarma@gmail.com>
2025-06-09 09:27:37 +0530

    dialog

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as Avatar } from "./Avatar";
 export { default as AlertDialog } from "./AlertDialog";
 export { default as Button } from "./Button";
 export { default as Checkbox } from "./Checkbox";
+export { default as Dialog } from "./Dialog";
---
+++ src/stories/Dialog.stories.tsx
@@ -0,0 +1,19 @@
+import { Dialog } from "../components";
+
+export default {
+  title: "Componets/Dialog",
+  component: Dialog,
+};
+
+export const Dialogs = () => {
+  return (
+    <div className="flex justify-center items-center h-full flex-col gap-4">
+      <Dialog name="Click me 1">
+        <h1>Do you really want to learn React</h1>
+      </Dialog>
+      <Dialog name="Click me 2" className="bg-red-600">
+        <h1 className="text-red-600">Do don't want to learn React</h1>
+      </Dialog>
+    </div>
+  );
+};
